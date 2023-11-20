b13518754ae874696a3bc3bec3e31d015f699fc4
M0NTANN <wot43ak@mail.ru>
2023-11-20 14:36:43 +0300

    v2

---
+++ VytIngineer/Schemas/UsrComp2Page/UsrComp2Page.js
@@ -0,0 +1,174 @@
+define("UsrComp2Page", [], function() {
+	return {
+		entitySchemaName: "UsrComp",
+		attributes: {},
+		modules: /**SCHEMA_MODULES*/{}/**SCHEMA_MODULES*/,
+		details: /**SCHEMA_DETAILS*/{
+			"Files": {
+				"schemaName": "FileDetailV2",
+				"entitySchemaName": "UsrCompFile",
+				"filter": {
+					"masterColumn": "Id",
+					"detailColumn": "UsrComp"
+				}
+			}
+		}/**SCHEMA_DETAILS*/,
+		businessRules: /**SCHEMA_BUSINESS_RULES*/{
+			"UsrName": {
+				"7ad5b507-085f-47dd-b8ef-ff570609c082": {
+					"uId": "7ad5b507-085f-47dd-b8ef-ff570609c082",
+					"enabled": true,
+					"removed": false,
+					"ruleType": 3,
+					"populatingAttributeSource": {
+						"expression": {
+							"type": 1,
+							"attribute": "UsrVidCard"
+						}
+					},
+					"logical": 0,
+					"conditions": [
+						{
+							"comparisonType": 2,
+							"leftExpression": {
+								"type": 1,
+								"attribute": "UsrVidCard"
+							}
+						}
+					]
+				}
+			}
+		}/**SCHEMA_BUSINESS_RULES*/,
+		methods: {},
+		dataModels: /**SCHEMA_DATA_MODELS*/{}/**SCHEMA_DATA_MODELS*/,
+		diff: /**SCHEMA_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "UsrName8fa0046c-986f-4557-9e80-22b09b7e5b26",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrName"
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "UsrComps285aaa12-1ed6-4b90-a0a7-2560045eaf33",
+				"values": {
+					"layout": {
+						"colSpan": 11,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrComps"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "UsrTypef7a1e950-3499-457d-b4bd-6d0a32cdc30a",
+				"values": {
+					"layout": {
+						"colSpan": 11,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 1,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrType"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "UsrVidCard4ff4d75b-522e-43df-905f-0004e21f09cc",
+				"values": {
+					"layout": {
+						"colSpan": 11,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 2,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrVidCard"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "UsrProc02e2a8e2-2035-43a4-9705-9637e5f0ceae",
+				"values": {
+					"layout": {
+						"colSpan": 11,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 3,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrProc"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 3
+			},
+			{
+				"operation": "insert",
+				"name": "UsrPriceIntf756123c-c0b0-492c-a41b-e6750f70613e",
+				"values": {
+					"layout": {
+						"colSpan": 11,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 4,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrPriceInt"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 4
+			},
+			{
+				"operation": "insert",
+				"name": "UsrNotes661d2406-ac48-4f9f-acd3-d817bd2b96c8",
+				"values": {
+					"layout": {
+						"colSpan": 11,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 5,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrNotes",
+					"enabled": true
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 5
+			},
+			{
+				"operation": "merge",
+				"name": "ESNTab",
+				"values": {
+					"order": 0
+				}
+			}
+		]/**SCHEMA_DIFF*/
+	};
+});
---
+++ VytIngineer/Schemas/UsrComp352373e8Section/UsrComp352373e8Section.js
@@ -0,0 +1,8 @@
+define("UsrComp352373e8Section", [], function() {
+	return {
+		entitySchemaName: "UsrComp",
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
+		methods: {}
+	};
+});
